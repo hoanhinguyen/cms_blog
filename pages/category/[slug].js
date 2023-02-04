37a68ad1de26f6ff966b09fb242d6597dd165d73
hoanhinguyen <88066895+hoanhinguyen@users.noreply.github.com>
2023-02-04 10:36:53 +0700

    fixing path

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useRouter } from 'next/router';
-import {server} from '../../config'
+import {server} from '../../config/index'
 
 
 import { getCategories, getCategoryPost } from '../../services';
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useRouter } from 'next/router';
-import {server} from '../../config'
+import {server} from '../../config/index'
 
 import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm, Loader } from '../../components';
 import { getPosts, getPostDetails } from '../../services';
